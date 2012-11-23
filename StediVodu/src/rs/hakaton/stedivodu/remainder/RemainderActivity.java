222e39648c4547c5b068d6117ba1f1452d768735
Zarko Jevtic <zarej@svrljig.net>
2012-11-23 19:31:48 +0100

    Sredjeni warning

--- StediVodu/src/rs/hakaton/stedivodu/remainder/RemainderActivity.java
+++ StediVodu/src/rs/hakaton/stedivodu/remainder/RemainderActivity.java
@@ -8,7 +8,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.util.Log;
 
 public class RemainderActivity extends Activity {
